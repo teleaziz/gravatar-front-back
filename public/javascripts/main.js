b750902dd5ae112b364cd2a72facc3cce63984b1
teleaziz <abdz.abbas@gmail.com>
2014-09-16 13:57:50 -0700

    testing mocha chai

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 var expect = chai.expect;
 var request = superagent;
-
+var testingCode ; 
 describe("Array.length", function() {
    it("returns number of elements in an array", function(){
       expect([2,3].length).to.equal(2);
@@ -22,4 +22,78 @@ describe("test api ", function() {
     expect(res.body.length).to.be.at.least(1);
   });
   });
+});
+
+
+describe("API creates a question ", function() {
+  it("Accepts email and question and creates a Question object" , function(done) {
+    request.post("api/questions")
+    .send({email:"test@test.com" , question: "test question"})
+    .end(function(err,res){
+      expect(res.status).to.equal(200);
+      expect(res.body.email).to.equal("test@test.com");
+      expect(res.body.question).to.equal("test question");
+      testingCode = res.body.code ; 
+      done();
+  });
+  });
+});
+
+
+describe("API lists all questions ", function() {
+  it("Returns a list of all questions" , function(done) {
+    request.get("api/questions")
+    .end(function(err,res){
+      expect(res.status).to.equal(200);
+      expect(res.body.length).to.be.at.least(1);
+      expect(res.body[0]).to.have.property("question");
+      done();
+  });
+  });
+});
+
+
+
+
+describe("API fetch a single question ", function() {
+  it("Returns a 404 for a non existing question" , function(done) {
+    request.get("api/questions/12930123123")
+    .end(function(err,res){
+      expect(res.status).to.equal(404);
+      done();
+  });
+  });
+  
+  it("Returns a questions object for valid question" , function(done) {
+    request.get("api/questions/"+testingCode)
+    .end(function(err,res){
+      expect(res.status).to.equal(200);
+      expect(res.body.question).to.equal("test question")
+      done();
+  });
+  });
+  
+});
+
+describe("API update a valid question ", function() {
+  it("Returns a 404 for a non existing question" , function(done) {
+    request.put("api/questions/12930123123")
+    .send({question:"hello I'm an updated question?"})
+    .end(function(err,res){
+      expect(res.status).to.equal(404);
+      done();
+  });
+  });
+  
+  it("Returns a questions object with the updated field" , function(done) {
+    request.put("api/questions/"+testingCode)
+    .send({question:"hello I am an updated question?"})
+    .end(function(err,res){
+      expect(res.status).to.equal(200);
+      expect(res.body.question).to.equal("hello I am an updated question?");
+      expect(res.body.email).to.equal("test@test.com");
+      done();
+  });
+  });
+  
 });
